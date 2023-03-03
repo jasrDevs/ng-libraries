9b45c22afeceb10df30fa8210121cf61b43e013f
ejdao <ejdao2019@hotmail.com>
2023-03-02 22:00:36 -0500

    PerfectScrollbar | Modulo agregado y publicado (v1.0.0)

---
+++ libraries/perfect-scrollbar/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of @eklipse/perfect-scrollbar
+ */
+export * from './perfect-scrollbar-force-native-scroll.directive';
+export * from './perfect-scrollbar.interfaces';
+export * from './perfect-scrollbar.component';
+export * from './perfect-scrollbar.directive';
+export * from './perfect-scrollbar.module';
